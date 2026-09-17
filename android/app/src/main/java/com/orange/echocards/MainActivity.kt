49dfe626ef9a8c6c298095f27f6cdcb82b3c8118
orange <xhconceit@163.com>
2026-09-18 02:01:09 +0800

    添加 Android 原生工程骨架并精简 .gitignore

---
+++ android/app/src/main/java/com/orange/echocards/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.orange.echocards
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(android.graphics.Color.TRANSPARENT, android.graphics.Color.TRANSPARENT),
+            navigationBarStyle = SystemBarStyle.light(android.graphics.Color.TRANSPARENT, android.graphics.Color.TRANSPARENT),
+        )
+        setContent {
+            Text(text = "helllo");
+        }
+    }
+}
+
+
